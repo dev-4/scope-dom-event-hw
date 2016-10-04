de37572a76daa5e09fddd0bb5f972056a07b99e7
dev-4 <dev.4@protonmail.com>
2016-10-04 16:00:28 -0400

    Added proper form

--- js/form.js
+++ js/form.js
@@ -1,1 +1,15 @@
-document.getElementById("text")
+function checkForm() {
+	var user = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	var correct = false;
+
+	if (password==12345678) {
+		if (user.length > 1) {
+			alert("Everything is fine");s
+		} else {
+			alert("Your username is invalid.");
+		}
+	} else {
+		alert("Your password is invalid.");
+	}
+}
